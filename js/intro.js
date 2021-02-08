775a3b3d7bc81796a8ccbcb46ed64be666a29bff
59013-76-11 <59013-76-11@exchange.ad.afpanet>
2021-02-08 16:55:46 +0100

    ajout de fichier js avancer

--- js/intro.js
+++ js/intro.js
@@ -1,15 +1,131 @@
-const x = 10;
+/*const x = 10;
 const t = 80;
 let y = 20;
 y = 41;
 z = y % 4
 
+y += 22;
 m=50
+
+var
 
 console.log(x)
 console.log(y)
 console.log(t)
 console.log(z)
-
-alert("PTDR")
          
+console.log("hahaha" + x)
+console.log(y)*/
+
+// let chaine = "premiere ligne \n deuxieme ligne"
+// let dossier = "dossier\\images\\image.png"
+// console.log(chaine);
+
+// console.log(dossier);
+
+// let person = {
+//     firstName: "John",
+//     lastName : "Doe",
+//     age : 35,
+//     id       : 5566,
+//     fullName : function() {
+//       return this.firstName + " " + this.lastName + " " + this.age + " years";
+//     }
+//   };
+
+//   console.log(person.lastName);
+
+//   console.log(person.id);
+
+//   console.log(person.fullName())
+
+
+//   function afficherMessage() {
+//       console.log("un message");
+// }
+
+// afficherMessage()
+
+
+// function somme (a,b)
+// {
+//     console.log(a+b);
+// }
+
+// somme(2,3)
+
+// somme(6,12)
+
+
+// function somme1 (a,b)
+// {return (a + b);
+// }
+
+// let x;
+// x = somme1(5, 6545);
+
+// console.log(x)
+
+
+
+// function somme2(a,b)
+// {
+//     return (a+b);
+// }
+
+// let k;
+// k = somme2(5, 7)
+
+// console.log(k);
+
+// let val1
+// val1 = Math.random() * 10;
+// console.log(val1);
+
+// let val2
+// val2 = Math.random() * 10
+// console.log(val2)
+
+// function sommeRandom(a,b)
+//     {return a+b;
+//     }
+
+// let s;
+// s = sommeRandom(val1, val2)
+
+// console.log(s);
+
+// let l;
+// l = Math.floor(Math.random() * 10)
+
+// console.log(l)
+
+function difference(a,b)
+    {
+        return(a == b);
+    }
+
+console.log(difference(40,10));
+
+let chaine2;
+chaine2 = "";
+console.log(chaine2);
+
+
+let j;
+j = 0;
+let u;
+u ="";
+while (j <= 9){
+    u += "*"
+    j++;
+    console.log(u)
+}
+
+console.log(1==="1")
+
+let a = 15;
+let b = 10;
+a<b ? x ="inferieur" : x ="superieur"
+
+console.log(x)
