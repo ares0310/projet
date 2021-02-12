fb5b092f81b1d411762d34c0975625654a984bcd
59013-76-11 <59013-76-11@exchange.ad.afpanet>
2021-02-12 17:00:38 +0100

    avance doucement

---
+++ JQuery/documentation/documentationjQ.js
@@ -0,0 +1,17 @@
+// $ ("selecteur") function(); --> les guillemets au sélecteurs important
+
+// Plusieurs events JQuery
+
+// $("p").on({
+//     mouseenter: function(){
+//       $(this).css("background-color", "lightgray");
+//     },
+//     mouseleave: function(){
+//       $(this).css("background-color", "lightblue");
+//     },
+//     click: function(){
+//       $(this).css("background-color", "yellow");
+//     }
+//   });
+
+// Jquery selector = CSS selector
